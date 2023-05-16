fc2ebe73c03e08073fab781318b01ffac1ac3199
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2023-05-16 16:31:51 +0300

    update

--- lib/build-labels/command_to_dockerfiles.rb
+++ lib/build-labels/command_to_dockerfiles.rb
@@ -9,8 +9,9 @@ BuildLabels::CommandLine::COMMANDS[:to_dockerfiles] = Class.new do
     compose['services'].each do |name, svc|
       next unless svc['build']
 
-      dockerfile = svc['build'].is_a?(String) ? svc['build'] : svc['build']['context']
-      dockerfile = File.join dockerfile, (svc['build']['dockerfile'] || 'Dockerfile')
+      # dockerfile = svc['build'].is_a?(String) ? svc['build'] : svc['build']['context']
+      # dockerfile = File.join dockerfile, (svc['build']['dockerfile'] || 'Dockerfile')
+      dockerfile = svc['build']['dockerfile'] || 'Dockerfile'
       dockerfile = File.expand_path dockerfile, compose_dir
       raise "file ot found: #{dockerfile}" unless File.exist? dockerfile
 

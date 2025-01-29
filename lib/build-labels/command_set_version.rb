5ca3643e4bc403814aef4ef5bdbf45cdfb4b18bc
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2025-01-29 14:23:43 +0300

    update

--- lib/build-labels/command_set_version.rb
+++ lib/build-labels/command_set_version.rb
@@ -5,7 +5,6 @@ BuildLabels::CommandLine::COMMANDS[:set_version] = Class.new do
   #   parser.on('', '--set-version', '')
   # end
 
-
   def apply_gitlab_tag(tag)
     return tag if ENV['CI_COMMIT_TAG'].to_s.empty?
     git_tag = ENV['CI_COMMIT_TAG']
@@ -22,20 +21,27 @@ BuildLabels::CommandLine::COMMANDS[:set_version] = Class.new do
       versionfile = svc['build'].is_a?(String) ? './' : svc['build']['context']
       versionfile = File.join versionfile, '.version'
       versionfile = File.expand_path versionfile, compose_dir
-      next unless File.exist? versionfile
-      current_version = File.read(versionfile).strip
+
+      current_version = File.exist?( versionfile) ? File.read(versionfile).strip : nil
       image = svc['image'].gsub( /:.*/, '')
       tag = svc['image'][/:(.*)/, 1]
 
       tag = apply_gitlab_tag(tag)
-      svc['image'] = "#{image}:#{current_version}#{tag ? "-" + tag : ""}"
+      full_tag = [current_version, tag].compact.join '-'
+      full_tag = full_tag.empty? ? ":#{full_tag}" : ''
+
+      svc['image'] = "#{image}#{full_tag}"
 
       next unless svc['build']['tags']
       svc['build']['tags'] = svc['build']['tags'].map do |t|
         image = t.gsub( /:.*/, '')
         tag = t[/:(.*)/, 1]
         tag = apply_gitlab_tag(tag)
-        "#{image}:#{current_version}#{tag ? "-" + tag : ""}"
+
+        full_tag = [current_version, tag].compact.join '-'
+        full_tag = full_tag.empty? ? ":#{full_tag}" : ''
+
+        "#{image}#{full_tag}"
       end
     end
 

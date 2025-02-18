70c744efcdef9838d0f528da8800afd4fddf99bf
Roman <pugachev.ra@yandex.ru>
2025-02-18 19:05:32 +0300

    Implementation of building labels in github actions

---
+++ lib/build-labels/command_github.rb
@@ -0,0 +1,42 @@
+require_relative 'command_line'
+
+BuildHUBels::CommandLine::COMMANDS[:githab] = Class.new do
+  def run(builder, params, compose_text)
+    builder.oc.vendor = 'GITHUB_SERVER_URL/$GITHUB_TRIGGERING_ACTOR'
+    builder.oc.authors = 'GITHUB_SERVER_URL/$GITHUB_TRIGGERING_ACTOR'
+    builder.oc.revision = '$GITHUB_SHA'
+    # builder.oc['ref.name'] = '$CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME'
+    builder.oc.source = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY'
+    builder.oc.documentation = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY'
+    builder.oc.licenses = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY'
+    builder.oc.url = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY'
+    # builder.oc.title = '$CI_PROJECT_TITLE'
+    # builder.oc.created = '$CI_JOB_STARTED_AT'
+    # builder.oc.version = '$CI_COMMIT_REF_NAME' # $CI_COMMIT_TAG
+
+    builder.add_namespace :github, 'com.github.ci'
+    builder.github.user = '$GITHUB_SERVER_URL/$GITHUB_TRIGGERING_ACTOR'
+    # builder.github.email = '$GITLAB_USER_EMAIL'
+    # builder.github.tagorbranch = '$CI_COMMIT_REF_NAME'
+    builder.github.actionurl = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID'
+    builder.github.commiturl = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/$GITHUB_SHA'
+    builder.github.cijoburl = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/jobs/$GITHUB_JOB'
+    builder.github.mrurl = '$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/pull/$GITHUB_PR_NUMBER${GITHUB_REF#refs/pull/}'
+    # builder.github.tag = '$CI_REGISTRY_IMAGE:$GITHUB_SHA'
+    builder.github.commit_branch = '$GITHUB_REF_NAME'
+    builder.github.commit_short_sha = '$GITHUB_SHA'
+    # builder.github.commit_timestamp = '$CI_COMMIT_TIMESTAMP'
+    # builder.github.pipeline_id = '$CI_PIPELINE_ID'
+    # builder.github.pipeline_iid = '$CI_PIPELINE_IID'
+
+    # org.opencontainers.image.title BUILDTITLE=$(echo $CI_PROJECT_TITLE | tr " " "_")
+    # org.opencontainers.image.description
+    # date '+%FT%T%z' | sed -E -n 's/(\+[0-9]{2})([0-9]{2})$/\1:\2/p' #rfc 3339 date
+    # org.opencontainers.image.created
+
+  end
+
+  def help = 'Use GitHub CI variables'
+end.new
+
+
--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 module BuildLabels
   class Builder
-    VERSION = '0.0.40'
+    VERSION = '0.0.41'
   end
 end
